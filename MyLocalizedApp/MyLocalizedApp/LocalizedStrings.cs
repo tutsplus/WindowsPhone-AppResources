5dbf4dfe849e7fda7035fc831fac861e2876b01f
InesAzouz <liounesaz24@gmail.com>
2015-06-21 09:22:57 +0100

    Initial commit

---
+++ MyLocalizedApp/MyLocalizedApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MyLocalizedApp.Resources;
+
+namespace MyLocalizedApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
